91aeed8ddaaeccf4cda25f3ca5adfa23b6b5a18e
lyrealmedia <145462808+lyrealmedia@users.noreply.github.com>
2023-10-03 01:19:56 +0800

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,19 +1,59 @@
 <?php
-//get data from form  
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$Name = $_POST['Name'];
-$Email= $_POST['Email'];
-$Number= $_POST['Number'];
-$Message= $_POST['Message'];
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
-$to = "lyrealmedia@gmail.com";
-$subject = "Lead Information from Vineyards Terrace Website";
-$txt ="Name = ". $Name . "\r\n  Email = " . $Email . "\r\n Number = " . $Number . "\r\n Message =" . $Message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+//get data from form
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$number= $_POST['number'];
+$message = $_POST['message'];
+
+// preparing mail content
+$messagecontent ="Name = ". $name . "<br>Email = " . $email . "<br>Number = ". $number .   "<br>Message =" . $message;
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'Lightfoot & Youngblood Investment Real Estate';                     //SMTP username
+    $mail->Password   = 'Gobig2023!';                               //SMTP password
+   // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('lyrealmedia@gmailcom', 'Mailer');
+    //$mail->addAddress('timsellsfutures@gmailcom', 'Joe User');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+   // $mail->addAttachment('photo.jpeg', 'photo.jpeg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Lead Information from Vineyards Terrace Website';
+    $mail->Body    = $messagecontent;
+    
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+   // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-//redirect
-header("Location:thankyou.html");
-?>
